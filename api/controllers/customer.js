01b855a05e5bf5fa1d94a650932d10b406ae95fc
Camila <camilacslopes@gmail.com>
2020-09-24 20:10:42 -0300

    configurando metodos put e delete

--- api/controllers/customer.js
+++ api/controllers/customer.js
@@ -1,4 +1,6 @@
-module.exports = () => {
+const uuidv4 = require('uuid/v4');
+
+module.exports = app => {
     const customerDB = app.data.customer;
     const controller = {};
 
@@ -23,7 +25,67 @@ module.exports = () => {
 
         res.status(201).json(customerMock);
     }
+   
+    controller.removeCustomer = (req,res) => {
+        const {
+            customerId,
+        } = req.params;
 
+        const foundCustomerIndex = customerMock.data.findIndex(customer => customer.id === customerId);
+        
+        if (foundCustomerIndex === -1) {
+            res.status(404).json({
+                message:'Cliente não encontrado na base de dados.',
+                success: false,
+                customer: customerMock,
+            });
+            
+        } else {
+            customerMock.data.splice(foundCustomerIndex, 1);
+            res.status(200).json({
+                message: 'Cliente deletado com sucesso!',
+                success: true,
+                customer: customerMock,
+            });
+        }
+    };
+
+    controller.updateCustomer = (req, res) => {
+        const {
+            customerId,
+        } = req.params;
+        
+        const foundCustomerIndex = customerMock.data.findIndex(customer => customer.id === customerId);
+        
+        if (foundCustomerIndex === -1) {
+            res.status(404).json({
+                message:'Cliente não encontrado na base de dados.',
+                success: false,
+                customer: customerMock,
+            });
+        } else {
+            const newCustomer = {
+                id: customerId,
+                parentId: req.body.parentId,
+                name: req.body.name,
+                birthDate: req.body.birthDate,
+                cellphone: req.body.cellphone,
+                phone: req.body.phone,
+                email: req.body.email,
+                occupation: req.body.occupation,
+                state: req.body.state,
+                createdAt: new Date()
+            };
+           
+           customerMock.data.splice(foundCustomerIndex, 1, newCustomer);
+           
+           res.status(200).json({
+               message: 'Dados Atualizados com sucesso!',
+               success: true,
+               customer: customerMock,
+           });
+        }
+    }
 
     return controller;
 }
--- api/routes/customer.js
+++ api/routes/customer.js
@@ -1,7 +1,12 @@
 module.exports = app => {
-    const controller = app.controller.customer;
+    const controller = app.controllers.customer;
 
     app.route('/api/v1/customer')
     .get(controller.listCustomer)
     .post(controller.saveCustomer);
+
+    app.route('/api/v1/customer/:customerId')
+    .delete(controller.removeCustomer)
+    .put(controller.updateCustomer);
 }
+
